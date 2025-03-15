2d90aedeccc5334f9e3cbe437555915efb5739b7
Joseph C <111119464+joseph-c100@users.noreply.github.com>
2025-03-15 21:03:19 +0000

    globe element from globe.gl

---
+++ src/lib/GlobeMap.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { onMount } from 'svelte';
+    import Globe from 'globe.gl';
+
+    let globeContainer
+    onMount(()=>{
+        const myGlobe = new Globe(globeContainer)
+    })
+</script>
+
+<div class="globeContainer" bind:this={globeContainer}></div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import GlobeMap from '$lib/GlobeMap.svelte'
+</script>
+
+<h1>Internet status globe</h1>
+<GlobeMap />
