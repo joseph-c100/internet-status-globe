56111b07ef853001e10d3d92465fe7df321298ba
Joseph C <111119464+joseph-c100@users.noreply.github.com>
2025-03-15 21:19:21 +0000

    globe container positioning

--- src/lib/GlobeMap.svelte
+++ src/lib/GlobeMap.svelte
@@ -1,11 +1,40 @@
 <script>
-    import { onMount } from 'svelte';
     import Globe from 'globe.gl';
-
-    let globeContainer
-    onMount(()=>{
-        const myGlobe = new Globe(globeContainer)
-    })
-</script>
-
-<div class="globeContainer" bind:this={globeContainer}></div>
+    import { onMount } from 'svelte';
+  
+    let globeContainer;
+  
+    onMount(() => {
+      const myGlobe = Globe()(globeContainer)
+      .globeImageUrl('//unpkg.com/three-globe/example/img/earth-night.jpg')
+        .backgroundColor('#fff')
+        .width(window.innerWidth) 
+        .height(window.innerHeight) 
+        .pointOfView({ lat: 0, lng: 0, altitude: 2 }); 
+  
+      // handle resizing
+      const handleResize = () => {
+        myGlobe.width(window.innerWidth).height(window.innerHeight);
+      };
+      window.addEventListener('resize', handleResize);
+  
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    });
+  </script>
+  
+  <style>
+    div {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100vw;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  </style>
+  
+  <div bind:this={globeContainer}></div>
+  
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,26 @@
     import GlobeMap from '$lib/GlobeMap.svelte'
 </script>
 
-<h1>Internet status globe</h1>
-<GlobeMap />
+<div class="container">
+    <div class="header">
+        <h1>Internet status globe</h1>
+    </div>
+    <div class="globe">
+        <GlobeMap />
+    </div>
+</div>
+
+
+<style>
+.header {
+    position: absolute;
+    z-index: 100;
+    padding: 1rem;
+}
+
+h1 {
+    font-size: 20px;
+    font-family: sans-serif;
+    font-weight: 400;
+}
+</style>
