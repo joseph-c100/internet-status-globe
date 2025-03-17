b95dcd0fea958650734f7f40f6c2efec499b4cc3
Joseph C <111119464+joseph-c100@users.noreply.github.com>
2025-03-17 10:10:59 +0000

    submarine cable paths

--- .svelte-kit/generated/client/nodes/1.js
+++ .svelte-kit/generated/client/nodes/1.js
@@ -1,1 +1,1 @@
-export { default as component } from "../../../../node_modules/@sveltejs/kit/src/runtime/components/svelte-5/error.svelte";
+export { default as component } from "../../../../node_modules/.pnpm/@sveltejs+kit@2.19.2_@sveltejs+vite-plugin-svelte@5.0.3_svelte@5.23.1_vite@6.2.2/node_modules/@sveltejs/kit/src/runtime/components/svelte-5/error.svelte";
--- src/lib/GlobeMap.svelte
+++ src/lib/GlobeMap.svelte
@@ -1,80 +1,80 @@
 <script>
-    import Globe from 'globe.gl';
-    import { onMount } from 'svelte';
-    import {last7days} from './index.js'
+  import Globe from "globe.gl";
+  import { onMount } from "svelte";
+  import { last7days } from "./index.js";
 
-    $: console.log($last7days)
-  
-    let globeContainer;
-    let myGlobe;
-  
-    onMount(() => {
-      myGlobe = Globe()(globeContainer)
-        .globeImageUrl('//unpkg.com/three-globe/example/img/earth-dark.jpg')
-        .backgroundColor('#000')
-        .width(window.innerWidth)
-        .height(window.innerHeight)
-        .pointOfView({ lat: 0, lng: 0, altitude: 1.2 })
-        .showAtmosphere(false);
-  
-      // getch boundary geojson data
-      fetch('/ne_110m_admin_0_countries.geojson')
-        .then(res => res.json())
-        .then(countries => {
-          myGlobe
-            .hexPolygonsData(countries.features)
-            .hexPolygonResolution(3)
-            .hexPolygonMargin(0.3)
-            .hexPolygonUseDots(false)
-            .hexPolygonColor(()=>'#fff')
-            .hexPolygonAltitude(0.02)
+  $: console.log($last7days);
+
+  let globeContainer;
+  let myGlobe;
+
+  onMount(() => {
+    myGlobe = Globe()(globeContainer)
+      // .globeImageUrl("//unpkg.com/three-globe/example/img/earth-dark.jpg")
+      .backgroundColor("#000")
+      .width(window.innerWidth)
+      .height(window.innerHeight)
+      .pointOfView({ lat: 0, lng: 0, altitude: 1.2 })
+      .showAtmosphere(false);
+
+    // getch boundary geojson data
+    fetch("/ne_110m_admin_0_countries.geojson")
+      .then((res) => res.json())
+      .then((countries) => {
+        myGlobe
+          .hexPolygonsData(countries.features)
+          .hexPolygonResolution(3)
+          .hexPolygonMargin(0.3)
+          .hexPolygonUseDots(false)
+          .hexPolygonColor(() => "#fff")
+          .hexPolygonAltitude(0.02);
+      });
+
+    // submarine internet cables geojson
+    fetch("/submarine_cables.json")
+      .then((res) => res.json())
+      .then((cablesGeo) => {
+        let cablePaths = [];
+        cablesGeo.features.forEach(({ geometry, properties }) => {
+          geometry.coordinates.forEach((coords) =>
+            cablePaths.push({ coords, properties })
+          );
         });
 
-        // submarine internet cables geojson
-      // fetch('https://www.submarinecablemap.com/api/v3/cable/cable-geo.json')
-      //   .then(res => res.json())
-      //   .then(cablesGeo => {
-      //   let cablePaths = [];
-      //   cablesGeo.features.forEach(({ geometry, properties }) => {
-      //     geometry.coordinates.forEach(coords => cablePaths.push({ coords, properties }));
-      //   });
+        myGlobe
+          .pathsData(cablePaths)
+          .pathPoints("coords")
+          .pathColor(() => "#cacaca")
+          .pathPointLat((p) => p[1])
+          .pathPointLng((p) => p[0])
+          .pathDashLength(0.1)
+          .pathDashGap(0.01)
+          .pathDashAnimateTime(12000);
+      });
 
-      //   myGlobe
-      //     .pathsData(cablePaths)
-      //     .pathPoints('coords')
-      //     .pathPointLat(p => p[1])
-      //     .pathPointLng(p => p[0])
-      //     .pathColor(path => path.properties.color)
-      //     .pathLabel(path => path.properties.name)
-      //     .pathDashLength(0.1)
-      //     .pathDashGap(0.008)
-      //     .pathDashAnimateTime(12000);
-      // });
-  
-      // Handle window resizing
-      const handleResize = () => {
-        myGlobe.width(window.innerWidth).height(window.innerHeight);
-      };
-      window.addEventListener('resize', handleResize);
-  
-      return () => {
-        window.removeEventListener('resize', handleResize);
-      };
-    });
-  </script>
-  
-  <style>
-    div {
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100vw;
-      height: 100vh;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
-  </style>
-  
-  <div bind:this={globeContainer}></div>
-  
+    // Handle window resizing
+    const handleResize = () => {
+      myGlobe.width(window.innerWidth).height(window.innerHeight);
+    };
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  });
+</script>
+
+<div bind:this={globeContainer}></div>
+
+<style>
+  div {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
